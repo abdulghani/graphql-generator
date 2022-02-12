02f97e3a59512c3e780711f590571dab658ba2f7
Abdul Ghani <contact.abdulghani@gmail.com>
2022-02-12 22:21:36 +0700

    chore: update merge script

--- .scripts/git-merge-branch.sh
+++ .scripts/git-merge-branch.sh
@@ -4,11 +4,13 @@ LIST_BRANCHES=($(echo $BRANCHES | tr ',' ' '))
 
 # FETCH ALL BRANCHES
 git fetch --all
+git pull origin $MERGE_WITH
 
 function merge_branch() {
     echo "MERGING ($1) BRANCH WITH ($MERGE_WITH)"
     git checkout "origin/$1"
-    git merge $MERGE_WITH
+    git pull "origin/$1"
+    git merge $MERGE_WITH --no-ff
     echo "BRANCH ($1) SUCCESSFULLY MERGED WITH ($MERGE_WITH)"
     git checkout $MERGE_WITH
 }
