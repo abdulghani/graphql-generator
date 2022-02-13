837424cca6416b27ce9b401eef854ed645784683
Abdul Ghani <contact.abdulghani@gmail.com>
2022-02-13 17:49:42 +0700

    chore: add package info in generated header

--- src/constants/default-graphql-sdl-file-header.ts
+++ src/constants/default-graphql-sdl-file-header.ts
@@ -2,6 +2,7 @@ const DEFAULT_GRAPHQL_SDL_FILE_HEADER = `
 #
 # -------------------------------------------------------
 # THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+# npm i -D @adgstudio/graphql-generator
 # -------------------------------------------------------
 #
 `.trim();
