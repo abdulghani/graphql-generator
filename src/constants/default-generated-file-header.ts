d3b513b77c76f354664c05dcd724d429e7db79dd
Abdul Ghani <contact.abdulghani@gmail.com>
2022-02-13 18:15:07 +0700

    chore: add version number to header

--- src/constants/default-generated-file-header.ts
+++ src/constants/default-generated-file-header.ts
@@ -1,8 +1,12 @@
+import { PACKAGE_INFO } from "./package-info";
+
 const DEFAULT_GENERATED_FILE_HEADER = `
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
- * npm i -D @adgstudio/graphql-generator
+ * npm i -D @adgstudio/graphql-generator (version ${
+   PACKAGE_INFO.version ?? "0.0.0"
+ })
  * -------------------------------------------------------
  */
 
